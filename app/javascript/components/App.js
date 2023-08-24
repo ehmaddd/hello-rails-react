cffaaed1019cbaff3a5e9dd4743b245b68c54303
ehmaddd <ehmaddd@gmail.com>
2023-08-24 15:34:31 +0500

    Add React to rails app

---
+++ app/javascript/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const App = () => {
+    return (
+        <h1>In the App</h1>
+    )
+}
+
+export default App;
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'greetings/random_greeting'
-  end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :roots, only: [:index]
+  root 'roots#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    get 'random_greeting', to: 'greetings#random_greeting'
+  end
 end
